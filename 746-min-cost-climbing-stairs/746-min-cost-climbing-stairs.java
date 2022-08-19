b0f431f0f249ab16cc77a9125fd72bbcdc8193f7
sankytech12 <99071299+sankytech12@users.noreply.github.com>
2022-08-19 18:05:48 +0545

    Time: 1 ms (89.83%), Space: 42.1 MB (88.96%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -2,7 +2,7 @@ class Solution {
     public int minCostClimbingStairs(int[] arr) {
         int dp[]=new int[arr.length];
 		Arrays.fill(dp, -1);
-		return helper(arr,dp);
+		return minCostBU(arr);
     }
     public static int helper(int []arr,int []dp) {
 		int first=minCost(arr, dp, 0);
@@ -20,5 +20,15 @@ class Solution {
 		int first=minCost(arr, dp, i+1);
 		int sec=minCost(arr, dp, i+2);
 		return dp[i]=Math.min(first, sec)+arr[i];
+	}
+    private static int minCostBU(int[] arr) {
+		// TODO Auto-generated method stub
+		int dp[]=new int[arr.length];
+		dp[0]=arr[0];
+		dp[1]=arr[1];
+		for (int i = 2; i < arr.length; i++) {
+			dp[i]=Math.min(dp[i-1], dp[i-2])+arr[i];
+		}
+		return Math.min(dp[dp.length-1],dp[dp.length-2]);
 	}
 }
