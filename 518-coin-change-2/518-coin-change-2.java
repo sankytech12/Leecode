38a685e145fb6b0ce8617965c78c80e3879a95b3
sankytech12 <99071299+sankytech12@users.noreply.github.com>
2022-08-21 23:40:23 +0545

    Time: 24 ms (20.11%), Space: 54.9 MB (5.86%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -4,7 +4,7 @@ class Solution {
 		for(int[] i:dp) {
 			Arrays.fill(i, -1);
 		}
-		return coinChange(arr,0,amt,dp);
+		return coinChangeBU(arr,amt);
     }
     
 	private static int coinChange(int[] arr, int i,int amt,int [][]dp) {
@@ -26,4 +26,24 @@ class Solution {
 		exc=coinChange(arr, i+1, amt,dp);
 		return dp[amt][i]=inc+exc;
 	}
+    private static int coinChangeBU(int[] arr,int amt) {
+		// TODO Auto-generated method stub
+		int [][]dp=new int[arr.length+1][amt+1];
+		for (int j = 0; j < dp.length; j++) {
+			dp[j][0]=1;
+		}
+		for (int i = 1; i < dp.length; i++) {
+			for (int w = 1; w < dp[0].length; w++) {
+				int inc=0;
+				int exc=0;
+				if(w>=arr[i-1]) {
+					inc=dp[i][w-arr[i-1]];
+				}
+				exc=dp[i-1][w];
+				dp[i][w]=inc+exc;
+			}
+		}
+		return dp[arr.length][amt];
+	}
+
 }
