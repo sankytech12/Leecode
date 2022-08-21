96040ead547a45f9453fbcaa665aa6570f492682
sankytech12 <99071299+sankytech12@users.noreply.github.com>
2022-08-22 00:23:27 +0545

    Time: 16 ms (55.78%), Space: 44.5 MB (75.88%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
@@ -1,18 +1,26 @@
 class Solution {
-    public int maxDotProduct(int[] nums1, int[] nums2) {
-        int n=nums1.length;
-		int m=nums2.length;
-		int [][]dp=new int[n][m];
+    public int maxDotProduct(int[] arr1, int[] arr2) {
+        
+		return maxProduct(arr1,arr2);
+    }
+   private static int maxProduct(int[] nums1, int[] nums2) {
+		// TODO Auto-generated method stub
+		int [][]dp=new int[nums1.length][nums2.length];
 		for (int i = 0; i < dp.length; i++) {
 			for (int j = 0; j < dp[0].length; j++) {
 				dp[i][j]=nums1[i]*nums2[j];
-				if (i > 0 && j > 0) dp[i][j] += Math.max(dp[i-1][j-1], 0);
-	              if (i > 0) dp[i][j] = Math.max(dp[i][j], dp[i-1][j]);
-	              if (j > 0) dp[i][j] = Math.max(dp[i][j], dp[i][j - 1]);
+				if(i==0&&j>0) {
+					dp[i][j]=Math.max(dp[i][j], dp[i][j-1]);
+				}
+				if(j==0&&i>0) {
+					dp[i][j]=Math.max(dp[i][j], dp[i-1][j]);
+				}
+				if(i>0&&j>0) {
+					dp[i][j]=Math.max(dp[i][j]+Math.max(0,dp[i-1][j-1]), Math.max(dp[i][j-1], dp[i-1][j]));
+				}
 			}
 		}
-		return dp[n-1][m-1];
-		
-    }
+		return dp[nums1.length-1][nums2.length-1];
+	}
     
 }
