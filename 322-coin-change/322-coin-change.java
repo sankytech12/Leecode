64c5e1bf6fcc27562c94bb6df719a8e773550a77
sankytech12 <99071299+sankytech12@users.noreply.github.com>
2022-08-23 22:15:15 +0545

    Time: 76 ms (14.87%), Space: 57.7 MB (15.32%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,6 +1,12 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        return minCoin(coins,amount);
+        int [][]dp=new int[coins.length+1][amount+1];
+        for(int i[]:dp) {
+			Arrays.fill(i, -1);
+		}
+		
+		return minco(coins,amount,0,dp)==Integer.MAX_VALUE-1?-1:minco(coins,amount,0,dp);
+        //return minCoin(coins,amount);
 
     }
     private static int minCoin(int[] arr, int sum) {
@@ -12,14 +18,14 @@ class Solution {
 		for (int i = 1; i < dp.length; i++) {
 			dp[i][0]=0;
 		}
-		// for (int i = 1; i < dp[0].length; i++) {
-		// 	if(i%arr[0]==0) {
-		// 		dp[1][i]=i/arr[0];
-		// 	}
-		// 	else {
-		// 		dp[1][i]=Integer.MAX_VALUE-1;
-		// 	}
-		// }
+		for (int i = 1; i < dp[0].length; i++) {
+			if(i%arr[0]==0) {
+				dp[1][i]=i/arr[0];
+			}
+			else {
+				dp[1][i]=Integer.MAX_VALUE-1;
+			}
+		}
 		for (int i = 1; i < dp.length; i++) {
 			for (int j = 1; j < dp[0].length; j++) {
 				
@@ -36,4 +42,24 @@ class Solution {
 		return dp[arr.length][sum]==Integer.MAX_VALUE-1?-1:dp[arr.length][sum];
 		
 	}
+    private static int minco(int[] arr, int sum, int i,int [][]dp) {
+		// TODO Auto-generated method stub
+		if(sum==0) {
+			return 0;
+		}
+		if(i==arr.length) {
+			return Integer.MAX_VALUE-1;
+		}
+		if(dp[i][sum]!=-1) {
+			return dp[i][sum];
+		}
+		int inc=Integer.MAX_VALUE-1;
+		int exc=0;
+		if(sum>=arr[i]) {
+			inc=1+minco(arr, sum-arr[i], i,dp);
+		}
+		exc=minco(arr, sum, i+1,dp);
+		return dp[i][sum]=Math.min(inc, exc);
+	}
+
 }
