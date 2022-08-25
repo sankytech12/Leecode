c6a054571e455f5ce8c12c0df30d445a24afb54d
sankytech12 <99071299+sankytech12@users.noreply.github.com>
2022-08-26 02:47:13 +0545

    Time: 3 ms (55.34%), Space: 49.6 MB (38.29%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,6 +1,7 @@
 class Solution {
     public int jump(int[] nums) {
-        return minS(nums);
+       // return minS(nums);
+        return minStep(nums,nums.length);
     }
     private static int minS(int[] arr) {
 		// TODO Auto-generated method stub
@@ -15,5 +16,36 @@ class Solution {
 			}
 		}
 		return jump;
+	}
+    
+	private static int  minStep(int[] arr,int n) {
+		// TODO Auto-generated method stub
+		if(n==1) {
+			return 0;
+		}
+		else if(arr[0]==0) {
+			return -1;
+		}
+		else {
+			int step=arr[0];
+			int maxR=arr[0];
+			int jump=1;
+			for (int i = 1; i < arr.length; i++) {
+				if(i==n-1) {
+					return jump;
+				}
+				maxR=Math.max(maxR, i+arr[i]);
+				step--;
+				if(step==0) {
+					jump++;
+					if(i>=maxR) {
+						return -1;
+					}
+					step=maxR-i;
+					
+				}
+			}
+			return -1;
+		}
 	}
 }
