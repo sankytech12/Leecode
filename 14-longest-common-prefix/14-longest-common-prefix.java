8c4e8eb529ecc8bd473af137b4e8f8e1a56ac54b
sankytech12 <99071299+sankytech12@users.noreply.github.com>
2022-08-28 01:42:15 +0545

    Time: 1 ms (89.13%), Space: 41.7 MB (77.14%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -8,6 +8,9 @@ class Solution {
 			while(strs[i].indexOf(prefix)!=0) {
 				prefix=prefix.substring(0, prefix.length()-1);
 			}
+            if(prefix.length()==0){
+                return "";
+            }
 		}
 		return prefix;
     }
