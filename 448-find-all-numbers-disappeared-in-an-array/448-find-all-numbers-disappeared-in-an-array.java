62f4f81e0ffd9bb336d21ca5ffbe565d5423abdf
sankytech12 <99071299+sankytech12@users.noreply.github.com>
2022-09-03 02:56:12 +0545

    Time: 5 ms (98.11%), Space: 66.3 MB (59.01%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.java
@@ -1,16 +1,25 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] arr) {
        List<Integer> ll=new ArrayList<Integer>();
-		HashMap<Integer, Integer> map=new HashMap<Integer, Integer>();
-		for (int i = 1; i <=arr.length; i++) {
-			map.put(i, 0);
-		}
+		// HashMap<Integer, Integer> map=new HashMap<Integer, Integer>();
+		// for (int i = 1; i <=arr.length; i++) {
+		// 	map.put(i, 0);
+		// }
+		// for (int i = 0; i < arr.length; i++) {
+		// 	map.put(arr[i], map.get(arr[i])+1);
+		// }		
+		// for(int key:map.keySet()) {
+		// 	if(map.get(key)==0) {
+		// 		ll.add(key);
+		// 	}
+		// }
+        int []frq=new int[arr.length+1];
 		for (int i = 0; i < arr.length; i++) {
-			map.put(arr[i], map.get(arr[i])+1);
-		}		
-		for(int key:map.keySet()) {
-			if(map.get(key)==0) {
-				ll.add(key);
+			frq[arr[i]]++;
+		}
+		for (int i = 1; i < frq.length; i++) {
+			if(frq[i]==0) {
+				ll.add(i);
 			}
 		}
 		return ll;
