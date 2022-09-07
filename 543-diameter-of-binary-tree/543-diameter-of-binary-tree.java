df172ef54f1971572a7e977d75da75c9d6dc346a
sankytech12 <99071299+sankytech12@users.noreply.github.com>
2022-09-07 18:45:54 +0545

    Time: 11 ms (12.68%), Space: 44 MB (12.76%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -18,20 +18,38 @@ public class Diapair {
 		int ht=0;
 	}
 	class Solution {
-		   public int diameterOfBinaryTree(TreeNode root) {
-			   return diameter(root).d;
-		   }
-		   public Diapair diameter(TreeNode root) {
-			   	if(root==null) {
-			   		return new Diapair();
-			   	}
+// 		   public int diameterOfBinaryTree(TreeNode root) {
+// 			   return diameter(root).d;
+// 		   }
+// 		   public Diapair diameter(TreeNode root) {
+// 			   	if(root==null) {
+// 			   		return new Diapair();
+// 			   	}
 			   	
-			   	Diapair ldp=diameter(root.left);
-			   	Diapair rdp=diameter(root.right);
-			   	Diapair sdp=new Diapair();
-			   	sdp.ht=Math.max(ldp.ht, rdp.ht)+1;
-			   	int dia=ldp.ht+rdp.ht;
-			   	sdp.d=Math.max(dia, Math.max(ldp.d, rdp.d));
-			   	return sdp;
-		   }
+// 			   	Diapair ldp=diameter(root.left);
+// 			   	Diapair rdp=diameter(root.right);
+// 			   	Diapair sdp=new Diapair();
+// 			   	sdp.ht=Math.max(ldp.ht, rdp.ht)+1;
+// 			   	int dia=ldp.ht+rdp.ht;
+// 			   	sdp.d=Math.max(dia, Math.max(ldp.d, rdp.d));
+// 			   	return sdp;
+// 		   }
+        public int diameterOfBinaryTree(TreeNode root) {
+		        if(root==null) {
+		        	return 0;
+		        }
+		        int ld=diameterOfBinaryTree(root.left);
+		        int rd=diameterOfBinaryTree(root.right);
+		        int sd=ht(root.left)+ht(root.right);
+		        return Math.max(ld, Math.max(rd, sd));
+		        
+		    }
+		    public int ht(TreeNode nn) {
+		    	if(nn==null) {
+		    		return 0;
+		    	}
+		    	int lh=ht(nn.left);
+		    	int rh=ht(nn.right);
+		    	return Math.max(lh, rh)+1;
+		    }
 	}
