0132d3c6ae54986ab00d5678585d136266a91a1a
sankytech12 <99071299+sankytech12@users.noreply.github.com>
2022-09-30 20:38:15 +0545

    Time: 0 ms (100.00%), Space: 60.7 MB (27.71%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -1,9 +1,23 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int ee=0;
-        for(int i=0;i<nums.length;i++){
-            ee=ee^nums[i];
+        // int ee=0;
+        // for(int i=0;i<nums.length;i++){
+        //     ee=ee^nums[i];
+        // }
+        // return ee;
+        return sin(nums);
+    }
+    public int sin(int []nums){
+        int lo=0,hi=nums.length-2;
+        while(lo<=hi){
+            int mid=(lo+hi)>>1;
+            if(nums[mid]==nums[mid^1]){
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
         }
-        return ee;
+        return nums[lo];
     }
 }
