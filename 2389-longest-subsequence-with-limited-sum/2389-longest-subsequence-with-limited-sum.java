85be335fc3d67655e0c9b94fb43440df8f8075d3
sankytech12 <99071299+sankytech12@users.noreply.github.com>
2022-10-01 20:46:08 +0545

    Time: 4 ms (99.94%), Space: 43 MB (88.75%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] answerQueries(int[] nums, int[] queries) {
-        return solve(nums,queries);
+       // return solve(nums,queries);
+        return solve2(nums,queries);
     }
     private static int[] solve(int[] n, int[] q) {
 		// TODO Auto-generated method stub
@@ -23,4 +24,17 @@ class Solution {
 		}
 		return ans;
 	}
+    private static int[] solve2(int []n,int []q){
+        Arrays.sort(n);
+        for(int i=1;i<n.length;i++){
+            n[i]=n[i]+n[i-1];
+        }
+        int res[]=new int[q.length];
+        for(int i=0;i<q.length;i++){
+            int j=Arrays.binarySearch(n,q[i]);
+            res[i]=Math.abs(j+1);
+            
+        }
+        return res;
+    }
 }
