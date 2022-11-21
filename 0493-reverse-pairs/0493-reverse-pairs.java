d003eb60a55f1f4e20d8bb557627a82e9207c70c
Sasank Sah <99071299+sankytech12@users.noreply.github.com>
2022-11-21 20:45:22 +0545

    Time: 59 ms (93.45%), Space: 71.8 MB (62.30%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,47 +1,51 @@
 class Solution {
-    public int reversePairs(int[] nums) {
-        return mergeSort(nums,0,nums.length-1);
-	}
-	private static int mergeSort(int[] arr, int i, int j) {
-		// TODO Auto-generated method stub
-		if(i>=j) return 0;
-		int count=0;
-		int mid=(i+j)/2;
-		count+=mergeSort(arr, i, mid);
-		count+=mergeSort(arr, mid+1, j);
-		count+=merge(arr,i,mid,j);
-		return count;
-	}
-	private static int merge(int[] arr, int low, int mid, int high) {
-		// TODO Auto-generated method stub
-		
-		int j=mid+1;
-		int count=0;
-		for (int i = low; i <=mid; i++) {
-			while(j<=high && arr[i]>(2*(long)(arr[j]))) {
-				j++;
-			}
-			count+=(j-(mid+1));
-		}
-		ArrayList<Integer> temp=new ArrayList<>();
-		int left=low,right=mid+1;
-		while(left<=mid && right<=high) {
-			if(arr[left]<=arr[right]) {
-				temp.add(arr[left++]);
-			}else {
-				temp.add(arr[right++]);
-			}
-		}
-		while(left<=mid) {
-			temp.add(arr[left++]);
-		}
-		while(right<=high) {
-			temp.add(arr[right++]);
-		}
-		for(int i=low;i<=high;i++) {
-			arr[i]=temp.get(i-low);
-		}
-		return count;
-	}
+    public int reversePairs(int[] arr) {
+        int []temp=new int[arr.length];
+        int ans=solve(arr,temp,0,arr.length-1);
+        return ans;
+    }
+
+    private static int solve(int[] arr,int []temp,int lo,int hi) {
+        if(lo>=hi) return 0;
+        int mid=(lo+hi)>>1;
+        int count=0;
+        count+=solve(arr,temp, lo, mid);
+        count+=solve(arr,temp, mid+1, hi);
+        count+=merge(arr,temp,lo,mid,hi);
+        return count;
+    }
+
+    private static int merge(int[] arr, int[] temp,int lo, int mid, int hi) {
+        //int i=lo;
+        int j=mid+1;
+        int count=0;
+        for(int i=lo;i<=mid;i++){
+            while(j<=hi && arr[i]>2*(long)(arr[j])){
+                j++;
+            }
+            count+=(j-(mid+1));
+        }
+        int k=lo;
+        int i=lo;
+        j=mid+1;
+        while(i<=mid && j<=hi){
+            if(arr[i]<=arr[j]){
+                temp[k++]=arr[i++];
+            }else{
+                
+                temp[k++]=arr[j++];
+            }
+        }
+        while(i<=mid){
+            temp[k++]=arr[i++];
+        }
+        while(j<=hi){
+            temp[k++]=arr[j++];
+        }
+        for (int l = lo; l <= hi; l++) {
+            arr[l]=temp[l];
+        }
+        return count;
+    }
 
 }
