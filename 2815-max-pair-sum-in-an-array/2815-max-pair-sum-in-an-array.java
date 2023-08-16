a445921509e553de533dfe358b05259571e5c5b0
Sasank Sah <99071299+sankytech12@users.noreply.github.com>
2023-08-16 17:06:57 +0530

    Time: 20 ms (56.70%), Space: 43.5 MB (82.52%) - LeetHub

--- 2815-max-pair-sum-in-an-array/2815-max-pair-sum-in-an-array.java
+++ 2815-max-pair-sum-in-an-array/2815-max-pair-sum-in-an-array.java
@@ -2,27 +2,22 @@ class Solution {
     public int maxSum(int[] nums) {
         return solve(nums);
     }
-    private static int solve(int[] arr) {
-        //int frq2[]=new int[10];
+     private static int solve(int[] arr) {
        int ans=-1;
         for (int i = 0; i < arr.length; i++) {
-            int frq1[]=new int[10];
             int num=arr[i];
             int max1=0;
             while(num>0){
                 int n=num%10;
                 if(n>max1) max1=n;
-                frq1[n]++;
                 num/=10;
             }
             for(int j=i+1;j<arr.length;j++){
-                int []frq2=new int[10];
                 int num2=arr[j];
                 int max2=0;
                 while(num2>0){
                     int n2=num2%10;
                     if(n2>max2) max2=n2;
-                    frq2[n2]++;
                     num2/=10;
                 }
                 if(max1==max2){
