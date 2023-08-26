2f0440910cc0e6205d80c30fabfbe96350182773
Sasank Sah <99071299+sankytech12@users.noreply.github.com>
2023-08-26 15:30:18 +0530

    Time: 49 ms (99.91%), Space: 44.5 MB (96.90%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -1,19 +1,23 @@
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        int []frq=new int[256];
-        int ei=0,si=0;
-        int ans=0;
-        while(ei<s.length()){
-            char ch=s.charAt(ei);
-            frq[ch]++;
-            while(frq[ch]>1){
-                char c=s.charAt(si);
-                frq[c]--;
-                si++;
-            }
-            ans=Math.max(ans,ei-si+1);
-            ei++;
+/**
+ * @param {string} s
+ * @return {number}
+};
+ */
+var lengthOfLongestSubstring = function(s) {
+    let ei=0;
+    let si=0;
+    let frq=new Array(256).fill(0);
+    let ans=0;
+    while(ei<s.length){
+        let c=s.charCodeAt(ei);
+        frq[c]++;
+        while(frq[c]>1){
+            let ch=s.charCodeAt(si);
+            frq[ch]--;
+            si++;
         }
-        return ans;
+        ans=Math.max(ans,ei-si+1);
+        ei++;
     }
-}
+    return ans;
+};
